cefab54a5e98fd22306dcd915aad2c4282e383ec
Max Zhao <mdcw1103@gmail.com>
2023-04-23 16:23:29 +0800

    chore: update deps

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, presetUno } from 'unocss'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({
   presets: [presetUno()],
+  transformers: [transformerVariantGroup()],
 })
