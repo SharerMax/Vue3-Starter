5261fd111a823b1d1d6c687581e601b04128f7ac
Max Zhao <mdcw1103@gmail.com>
2024-01-29 00:16:48 +0800

    fix: eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import process from 'node:process'
 import antfu from '@antfu/eslint-config'
 
 export default antfu(
@@ -5,15 +6,15 @@ export default antfu(
     // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
     files: ['**/*.vue'],
     rules: {
-      'vue/component-tags-order': ['error', {
+      'vue/block-order': ['error', {
         order: ['template', 'script', 'style'],
-      }]
+      }],
     },
   },
   {
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    }
-  }
+    },
+  },
 )
