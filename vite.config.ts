bda7ec977be7dc085cab3343b2712bf4854e59ee
Max Zhao <mdcw1103@gmail.com>
2025-02-18 18:53:51 +0800

    chore: update deps and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import * as path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 
@@ -16,13 +18,16 @@ export default defineConfig({
   },
   css: {
     transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist()),
+    },
   },
   build: {
     cssMinify: 'lightningcss',
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name?.split('.')[1]
+          let extType = assetInfo.names[0]?.split('.')[1]
           if (extType && /png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType))
             extType = 'img'
           return `assets/${extType}/[name]-[hash][extname]`
